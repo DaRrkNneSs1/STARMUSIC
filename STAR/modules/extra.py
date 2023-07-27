835a36daaff0a8a7e95326cd2285f66401d654ab
DaRrkNneSs1 <108493023+DaRrkNneSs1@users.noreply.github.com>
2023-07-27 22:31:07 +0300

    Update extra.py

--- STAR/modules/extra.py
+++ STAR/modules/extra.py
@@ -139,26 +139,69 @@ async def iddopen(client, message):
 
 
 
-@Client.on_message(
-    command(["ايدي","id","ا"])
-    & filters.group
-    & ~filters.edited
-)
-async def iddd(client, message):
-    if message.chat.id in iddof:
-      return
-    usr = await client.get_chat(message.from_user.id)
-    name = usr.first_name
-    photo = await client.download_media(usr.photo.big_file_id)
-    await message.reply_photo(photo,       caption=f"""⤹ NaMe. : {message.from_user.mention}\n ⤹ UseR. : @{message.from_user.username}\n ⤹ Id. : `{message.from_user.id}`\n ⤹ Bio. :{usr.bio}\n ⤹ ChaT: {message.chat.title}\n⤹ iD GrouP : `{message.chat.id}`""", 
-    reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        name, url=f"https://t.me/{message.from_user.username}")
-                ],
-            ]
-        ),
+@Client.on_message(command(["ا", "ايدي"]))
+async def who_is(client, message):
+    """انتظر لحظه !"""
+    status_message = await message.reply_text(
+        "انتظر لحظه !"
+    )
+    from_user = None
+    from_user_id, _ = extract_user(message)
+    try:
+        from_user = await client.get_users(from_user_id)
+    except Exception as error:
+        await status_message.edit(str(error))
+        return
+    if from_user is None:
+        await status_message.edit("no valid user_id / message specified")
+        return
+    
+    first_name = from_user.first_name or ""
+    last_name = from_user.last_name or ""
+    username = from_user.username or ""
+    
+    message_out_str = (
+        "<b>⋆ 𝑺𝒐𝒖𝒓𝒄𝒆 𝑺𝒕𝒂𝒓 ⋆</b>\n\n"
+        f"<b>• Name : <a href='tg://user?id={from_user.id}'>{first_name}</a></b>\n"
+        f"<b>• ID :</b> <code>{from_user.id}</code>\n"
+        f"<b>• User :</b> @{username}\n"
+        f"<b>• Bio :</b> {usr.bio}
+        f"<b>Is Deleted:</b> True\n" if from_user.is_deleted else ""
+        f"<b>Is Verified:</b> True" if from_user.is_verified else ""
+        f"<b>Is Scam:</b> True" if from_user.is_scam else ""
+        # f"<b>Is Fake:</b> True" if from_user.is_fake else "
     )
 
-
+    if message.chat.type in ["supergroup", "channel"]:
+        try:
+            chat_member_p = await message.chat.get_member(from_user.id)
+            joined_date = datetime.fromtimestamp(
+                chat_member_p.joined_date or time.time()
+            ).strftime("%Y.%m.%d %H:%M:%S")
+            message_out_str += (
+                "<b>• Lastseen :</b> <code>"
+                f"{joined_date}"
+                "</code>\n"
+                "-"
+            )
+        except UserNotParticipant:
+            pass
+    chat_photo = from_user.photo
+    if chat_photo:
+        local_user_photo = await client.download_media(
+            message=chat_photo.big_file_id
+        )
+        await message.reply_photo(
+            photo=local_user_photo,
+            quote=True,
+            caption=message_out_str,
+            disable_notification=True
+        )
+        os.remove(local_user_photo)
+    else:
+        await message.reply_text(
+            text=message_out_str,
+            quote=True,
+            disable_notification=True
+        )
+    await status_message.delete()
