3d4ffb98819d75cc3bcff1ce28ae5baf5d300630
DaRrkNneSs1 <108493023+DaRrkNneSs1@users.noreply.github.com>
2023-07-28 02:10:54 +0300

    Update devb.py

--- STAR/modules/devb.py
+++ STAR/modules/devb.py
@@ -21,7 +21,7 @@ from STAR.config import OWNER_USERNAME
     & ~filters.edited
 )
 async def yas(client, message):
-    usr = await client.get_chat({OWNER_USERNAME})
+    usr = await client.get_chat(@{OWNER_USERNAME})
     name = usr.first_name
     photo = await client.download_media(usr.photo.big_file_id)
     await message.reply_photo(photo,       caption=f"⋆ 𝑺𝒐𝒖𝒓𝒄𝒆 𝑺𝒕𝒂𝒓 ⋆ **\n\n‍ ⤹ DeV . :{name}\n ⤹ UsEr . :@{usr.username}\n ⤹ Id . :{usr.id}\n ⤹ Bio . :{usr.bio}\n\n ⤹ DeV ChanneL. @{UPDATE} **", 
